1a51eeffa429b594a39e6d31525aded5a12361cc
donnv <donnv@ncb-bank.vn>
2026-03-25 18:07:43 +0700

    [donnv] delete unuse package

--- packages/flutter_whatsnew/example/android/app/src/main/kotlin/com/example/flutter_whatsnew_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_whatsnew_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- packages/undo/lib/undo.dart
+++
@@ -1,5 +0,0 @@
-library undo;
-
-export 'src/change.dart';
-export 'src/undo_stack.dart';
-export 'src/simple_stack.dart';
