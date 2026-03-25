ae2f1fca382bf05b868b7fa23b50c8bf5cfa7fc0
donnv <donnv@ncb-bank.vn>
2026-03-25 18:57:18 +0700

    [donnv] fix

--- packages/flutter_sms/ios/Classes/SwiftFlutterSmsPlugin.swift
+++ packages/flutter_sms/ios/Classes/SwiftFlutterSmsPlugin.swift
@@ -17,24 +17,37 @@ public class SwiftFlutterSmsPlugin: NSObject, FlutterPlugin, SmsHostApi, UINavig
           code: "message_not_sent",
           message: "Cannot send message on this device!",
           details: "Cannot send SMS and MMS on a Simulator. Test on a real device."
-        )))
-    #else
+      )))
+#else
       if (MFMessageComposeViewController.canSendText()) {
-        self.result = completion
-        let controller = MFMessageComposeViewController()
-        controller.body = message
-        controller.recipients = recipients
-        controller.messageComposeDelegate = self
-        UIApplication.shared.keyWindow?.rootViewController?.present(controller, animated: true, completion: nil)
-      } else {
-        completion(.failure(PigeonError(
-            code: "device_not_capable",
-            message: "The current device is not capable of sending text messages.",
-            details: "A device may be unable to send messages if it does not support messaging or if it is not currently configured to send messages. This only applies to the ability to send text messages via iMessage, SMS, and MMS."
-          )))
+          self.result = completion
+          let controller = MFMessageComposeViewController()
+          controller.body = message
+          controller.recipients = recipients
+          controller.messageComposeDelegate = self
+
+          DispatchQueue.main.async {
+              if let topVC = UIApplication.topViewController() {
+                  topVC.present(controller, animated: true, completion: nil)
+              } else {
+                completion(.failure(PigeonError(
+                  code: "no_presentation_context",
+                  message: "Unable to present SMS composer.",
+                  details: "No visible UIViewController found in the window hierarchy. Ensure the app has an active screen before attempting to send SMS."
+                )))
+              }
+          }
+
+
+          } else {
+              completion(.failure(PigeonError(
+                code: "device_not_capable",
+                message: "The current device is not capable of sending text messages.",
+                details: "A device may be unable to send messages if it does not support messaging or if it is not currently configured to send messages. This only applies to the ability to send text messages via iMessage, SMS, and MMS."
+              )))
+          }
+#endif
       }
-    #endif
-  }
 
   public func canSendSms(completion: @escaping (Result<Bool, Error>) -> Void) {
     #if targetEnvironment(simulator)
@@ -57,6 +70,37 @@ public class SwiftFlutterSmsPlugin: NSObject, FlutterPlugin, SmsHostApi, UINavig
     if let callback = self.result {
         callback(.success(map[result] ?? "unknown"))
     }
-    UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+
+     DispatchQueue.main.async {
+        controller.dismiss(animated: true, completion: nil)
+    }
   }
 }
+
+extension UIApplication {
+    static func topViewController(
+        base: UIViewController? =
+            UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first { $0.isKeyWindow }?
+                .rootViewController
+    ) -> UIViewController? {
+
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+
+        if let tab = base as? UITabBarController {
+            return topViewController(base: tab.selectedViewController)
+        }
+
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+
+        return base
+    }
+}
+
+ 
