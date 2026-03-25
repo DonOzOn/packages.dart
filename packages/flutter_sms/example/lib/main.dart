f6cb78ae3c5619f2912468d810e61f83fe226682
donnv <donnv@ncb-bank.vn>
2026-03-25 18:59:41 +0700

    update

--- packages/flutter_sms/example/lib/main.dart
+++ packages/flutter_sms/example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_sms/flutter_sms.dart';
+import 'package:send_sms_handle/send_sms_handle.dart';
 
 void main() {
   runApp(const SmsApp());
